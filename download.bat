173a18bc14434d3c5db42b1a319155993a888bc3
User <gaze9999@gmail.com>
2018-12-24 18:01:20 +0800

    fix_dl

--- download.bat
+++ download.bat
@@ -1,7 +1,23 @@
 @ECHO off
-title Installing Office 2019
+title Download Office 2019
 
+IF EXIST temp RMDIR /s /q temp
+MKDIR temp
 IF NOT EXIST %~dp0\oitpinstall MKDIR %~dp0\oitpinstall
+IF NOT EXIST %~dp0\dllog MKDIR %~dp0\dllog
+
+:init
+ECHO ^<Configuration^> > temp\dl_configure.xml
+ECHO. >> temp\dl_configure.xml
+ECHO ^<Add SourcePath="%CD%\oitpinstall\" OfficeClientEdition="32" ^> >> temp\dl_configure.xml
+ECHO     ^<Product ID="ProPlus2019Retail"^> >> temp\dl_configure.xml
+ECHO	    ^<Language ID="zh-tw" /^> >> temp\dl_configure.xml
+ECHO     ^</Product^> >> temp\dl_configure.xml
+ECHO   ^</Add^> >> temp\dl_configure.xml
+ECHO. >> temp\dl_configure.xml
+ECHO ^<Logging Name="OfficeDL.txt" Path="%CD%\dllog\" /^> >> temp\dl_configure.xml
+ECHO. >> temp\dl_configure.xml
+ECHO ^</Configuration^> >> temp\dl_configure.xml
 
 :begin
 CLS
@@ -11,17 +27,16 @@ SET /P INPUT=Press Y to Download newest office, N to cancel.
 ECHO.
 ECHO.
 IF /I '%INPUT%'=='Y' GOTO download
-IF /I '%INPUT%'=='N' ( 
-GOTO End2
-)
+IF /I '%INPUT%'=='N' GOTO End2
 
 :download
 CLS
 ECHO =========DOWNLOADING OFFICE==========
-CALL Setup.exe /download Configuration.xml
+CALL %CD%\setup.exe /download %CD%\temp\dl_configure.xml
 goto end1
 
 :End1
+RMDIR /s /q temp
 CLS
 ECHO =========DOWNLOAD COMPLETED==========
 ECHO.
@@ -29,6 +44,7 @@ Pause
 Exit
 
 :End2
+RMDIR /s /q temp
 CLS
 ECHO =========DOWNLOAD CANCELED===========
 ECHO.
--- install.bat
+++ install.bat
@@ -6,23 +6,21 @@ MKDIR %TEMP%\oitplog
 
 :install
 ECHO ^<Configuration^> > %CD%\oitp_configure.xml
-ECHO. >> %TEMP%\oitp\oitp_configure.xml
-ECHO ^<Add SourcePath="%CD%\oitpinstall\" OfficeClientEdition="32" ^> >> %CD%\oitp_configure.xml
-ECHO     ^<Product ID="Standard2019Volume"^> >> %CD%\oitp_configure.xml
+ECHO. >> %CD%\oitp_configure.xml
+ECHO   ^<Add SourcePath="%CD%\oitpinstall\" OfficeClientEdition="32" Channel="Monthly" ^> >> %CD%\oitp_configure.xml
+ECHO     ^<Product ID="Standard2019Retail"^> >> %CD%\oitp_configure.xml
 ECHO        ^<Language ID="zh-tw" /^> >> %CD%\oitp_configure.xml
-ECHO		^<ExcludeApp ID="Groove" /^> >> %CD%\oitp_configure.xml
-ECHO        ^<ExcludeApp ID="OneNote" /^> >> %CD%\oitp_configure.xml
 ECHO     ^</Product^> >> %CD%\oitp_configure.xml
 ECHO   ^</Add^> >> %CD%\oitp_configure.xml
 ECHO. >> %CD%\oitp_configure.xml
-ECHO ^<Property Name="SharedComputerLicensing" Value="0" /^> >> %CD%\oitp_configure.xml
-ECHO ^<Property Name="PinIconsToTaskbar" Value="TRUE" /^> >> %CD%\oitp_configure.xml
-ECHO ^<Property Name="SCLCacheOverride" Value="0" /^> >> %CD%\oitp_configure.xml
-ECHO ^<Updates Enabled="TRUE" /^> >> %CD%\oitp_configure.xml
-ECHO ^<RemoveMSI All="TRUE" /^> >> %CD%\oitp_configure.xml
-ECHO ^<Display Level="None" AcceptEULA="FALSE" /^> >> %CD%\oitp_configure.xml
-ECHO ^<Logging Name="OfficeSetup.txt" Path="%TEMP%\oitplog\" /^> >> %CD%\oitp_configure.xml
-ECHO. >> %TEMP%\oitp\oitp_configure.xml
+ECHO     ^<Property Name="SharedComputerLicensing" Value="0" /^> >> %CD%\oitp_configure.xml
+ECHO     ^<Property Name="PinIconsToTaskbar" Value="TRUE" /^> >> %CD%\oitp_configure.xml
+ECHO     ^<Property Name="SCLCacheOverride" Value="0" /^> >> %CD%\oitp_configure.xml
+ECHO     ^<Updates Enabled="TRUE" Channel="Monthly" /^> >> %CD%\oitp_configure.xml
+ECHO     ^<RemoveMSI All="TRUE" /^> >> %CD%\oitp_configure.xml
+ECHO     ^<Display Level="None" AcceptEULA="FALSE" /^> >> %CD%\oitp_configure.xml
+ECHO     ^<Logging Name="OfficeSetup.txt" Path="%TEMP%\oitplog\" /^> >> %CD%\oitp_configure.xml
+ECHO. >> %CD%\oitp_configure.xml
 ECHO ^</Configuration^> >> %CD%\oitp_configure.xml
 CLS
 
@@ -38,5 +36,5 @@ CLS
 ECHO ==========INSTALL COMPELTE===========
 ECHO -------------------------------------
 ECHO ========PRESS ANY KEY TO EXIT========
-Pause>null
+Pause > NUL
 Exit
