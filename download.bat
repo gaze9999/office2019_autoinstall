281805d9f10c7a065965c64840dc151f6d3be5a3
User <gaze9999@gmail.com>
2019-02-25 19:44:42 +0800

    change_to_currect_folder

--- download.bat
+++ download.bat
@@ -7,17 +7,17 @@ IF NOT EXIST %~dp0\oitpinstall MKDIR %~dp0\oitpinstall
 IF NOT EXIST %~dp0\dllog MKDIR %~dp0\dllog
 
 :init
-ECHO ^<Configuration^> > temp\dl_configure.xml
-ECHO. >> temp\dl_configure.xml
-ECHO ^<Add SourcePath="%CD%\oitpinstall\" OfficeClientEdition="32" ^> >> temp\dl_configure.xml
-ECHO     ^<Product ID="ProPlus2019Retail"^> >> temp\dl_configure.xml
-ECHO	    ^<Language ID="zh-tw" /^> >> temp\dl_configure.xml
-ECHO     ^</Product^> >> temp\dl_configure.xml
-ECHO   ^</Add^> >> temp\dl_configure.xml
-ECHO. >> temp\dl_configure.xml
-ECHO ^<Logging Name="OfficeDL.txt" Path="%CD%\dllog\" /^> >> temp\dl_configure.xml
-ECHO. >> temp\dl_configure.xml
-ECHO ^</Configuration^> >> temp\dl_configure.xml
+ECHO ^<Configuration^> > %~dp0\dl_configure.xml
+ECHO. >> %~dp0\dl_configure.xml
+ECHO ^<Add SourcePath="%CD%\oitpinstall\" OfficeClientEdition="32" ^> >> %~dp0\dl_configure.xml
+ECHO     ^<Product ID="ProPlus2019Retail"^> >> %~dp0\dl_configure.xml
+ECHO	    ^<Language ID="zh-tw" /^> >> %~dp0\dl_configure.xml
+ECHO     ^</Product^> >> %~dp0\dl_configure.xml
+ECHO   ^</Add^> >> %~dp0\dl_configure.xml
+ECHO. >> %~dp0\dl_configure.xml
+ECHO ^<Logging Name="OfficeDL.txt" Path="%~dp0\dllog\" /^> >> %~dp0\dl_configure.xml
+ECHO. >> %~dp0\dl_configure.xml
+ECHO ^</Configuration^> >> %~dp0\dl_configure.xml
 
 :begin
 CLS
@@ -32,7 +32,7 @@ IF /I '%INPUT%'=='N' GOTO End2
 :download
 CLS
 ECHO =========DOWNLOADING OFFICE==========
-CALL %CD%\setup.exe /download %CD%\temp\dl_configure.xml
+setup.exe /download %~dp0\dl_configure.xml
 goto end1
 
 :End1
